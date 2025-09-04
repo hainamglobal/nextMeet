dc5df1b7c1bb4be324d19c03c980702ac36e5b0f
Bread Genie <msuhailbh07@gmail.com>
2025-09-04 16:26:51 +0530

    fix: layout when there are 16+ users

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,7 @@ export default {
 		"./src/**/*.{vue,js,ts,jsx,tsx}",
 		"./node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
 	],
+	safelist: ["grid-cols-1", "grid-cols-2", "grid-cols-3", "grid-cols-4"],
 	theme: {
 		extend: {},
 	},
