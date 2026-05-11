eab3016106e13ec8309c24e9a8fa012ca2bc031c
DAT-cy <dat224k@gmail.com>
2026-05-11 10:14:42 +0000

    fix : config server

--- meet/utils/sfu_config.py
+++ meet/utils/sfu_config.py
@@ -6,8 +6,16 @@ import frappe
 
 def get_sfu_config():
 	"""Get SFU configuration from site config or defaults"""
+	site_host = frappe.conf.get("host_name")
+	sfu_server_port = frappe.conf.get("socketio_port")
+	if ":" in site_host.replace("://", ""):
+		from urllib.parse import urlparse
+
+		parsed = urlparse(site_host)
+		site_host = f"{parsed.scheme}://{parsed.hostname}"
+
 	return {
-		"sfu_server_url": frappe.conf.get("sfu_server_url", "http://localhost"),
-		"sfu_server_port": frappe.conf.get("sfu_server_port", 3000),
+		"sfu_server_url": frappe.conf.get("sfu_server_url", site_host),
+		"sfu_server_port": frappe.conf.get("sfu_server_port", sfu_server_port),
 		"sfu_secret": frappe.conf.get("sfu_secret", ""),
 	}
